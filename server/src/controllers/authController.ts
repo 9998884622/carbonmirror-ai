dae11486937c06d4f5072cd345662452b2b07f2d
priya <priya1722006@gmail.com>
2026-06-09 18:57:45 +0530

    Update authController.ts

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,1 +1,101 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
 
+import User from "../models/User";
+import { generateToken } from "../utils/generateToken";
+
+import {
+  registerSchema,
+  loginSchema
+} from "../validators/authValidator";
+
+export const registerUser = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const data = registerSchema.parse(req.body);
+
+    const existingUser = await User.findOne({
+      email: data.email
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists"
+      });
+    }
+
+    const hashedPassword =
+      await bcrypt.hash(data.password, 10);
+
+    const user = await User.create({
+      name: data.name,
+      email: data.email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      success: true,
+      token: generateToken(user._id.toString()),
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+export const loginUser = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const data = loginSchema.parse(req.body);
+
+    const user = await User.findOne({
+      email: data.email
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials"
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      data.password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials"
+      });
+    }
+
+    res.json({
+      success: true,
+      token: generateToken(user._id.toString()),
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message
+    });
+  }
+};

51a954fd7ab04cf2fea40e18b2ef8858edd58dc1
priya <priya1722006@gmail.com>
2026-06-09 19:02:27 +0530

    Update recommendationService.ts

--- server/src/services/recommendationService.ts
+++ server/src/services/recommendationService.ts
@@ -1,1 +1,37 @@
+export const getRecommendations = (
+  transportKm: number,
+  electricityUnits: number,
+  foodType: string
+) => {
+  const recommendations: string[] = [];
 
+  if (transportKm > 20) {
+    recommendations.push(
+      "Use public transport twice a week."
+    );
+  }
+
+  if (electricityUnits > 200) {
+    recommendations.push(
+      "Reduce AC usage by 1 hour daily."
+    );
+  }
+
+  if (
+    foodType === "non-vegetarian"
+  ) {
+    recommendations.push(
+      "Try one vegetarian day per week."
+    );
+  }
+
+  if (
+    recommendations.length === 0
+  ) {
+    recommendations.push(
+      "Great job. Maintain your sustainable lifestyle."
+    );
+  }
+
+  return recommendations;
+};

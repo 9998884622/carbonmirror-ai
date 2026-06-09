2a964b6f49a86841855ea3a76e92a046840ef0c0
priya <priya1722006@gmail.com>
2026-06-09 18:58:58 +0530

    Update userRoutes.ts

--- server/src/ routes/userRoutes.ts
+++ server/src/ routes/userRoutes.ts
@@ -1,1 +1,21 @@
+import { Router } from "express";
 
+import {
+  protect,
+  AuthRequest
+} from "../middleware/authMiddleware";
+
+const router = Router();
+
+router.get(
+  "/profile",
+  protect,
+  async (req: AuthRequest, res) => {
+    res.json({
+      success: true,
+      user: req.user
+    });
+  }
+);
+
+export default router;

662b2dcae488bb50dfc6f76df544044d8e57aa1b
priya <priya1722006@gmail.com>
2026-06-09 20:08:15 +0530

    Create Simulator.tsx

---
+++ client/src/pages/Simulator.tsx
@@ -0,0 +1,15 @@
+function Simulator() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">
+        Future Carbon Simulator
+      </h1>
+
+      <p className="mt-4">
+        Coming Soon...
+      </p>
+    </div>
+  );
+}
+
+export default Simulator;

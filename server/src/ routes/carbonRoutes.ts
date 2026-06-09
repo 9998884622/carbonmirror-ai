3109bc9d234b1e1959099323352e4ef7b1cabca6
priya <priya1722006@gmail.com>
2026-06-09 19:03:43 +0530

    Update carbonRoutes.ts

--- server/src/ routes/carbonRoutes.ts
+++ server/src/ routes/carbonRoutes.ts
@@ -1,1 +1,19 @@
+import { Router } from "express";
 
+import {
+  calculateCarbon
+} from "../controllers/carbonController";
+
+import {
+  protect
+} from "../middleware/authMiddleware";
+
+const router = Router();
+
+router.post(
+  "/calculate",
+  protect,
+  calculateCarbon
+);
+
+export default router;

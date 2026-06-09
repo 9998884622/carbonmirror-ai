4010b86fb8c608c9a01f48a29eb3453445ef62fd
priya <priya1722006@gmail.com>
2026-06-09 20:32:41 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,7 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import app from "./app";
-import { connectDB } from "./config/db";
 
 const PORT = process.env.PORT || 5000;
 
-const startServer = async () => {
-  await connectDB();
-
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-};
-
-startServer();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

41d81909a431c54e0bf8c4d4b7692bb60a81ffaf
priya <priya1722006@gmail.com>
2026-06-09 19:21:59 +0530

    Create App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen">
+        <h1 className="text-4xl font-bold text-center mt-20">
+          CarbonMirror AI
+        </h1>
+
+        <p className="text-center mt-4">
+          Predict • Reduce • Sustain
+        </p>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;

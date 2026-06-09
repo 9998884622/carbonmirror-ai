da771765052022f79ec248aded920dc0ef9b902a
priya <priya1722006@gmail.com>
2026-06-09 20:06:16 +0530

    Update Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import { Leaf } from "lucide-react";
 
 function Navbar() {
   return (
     <nav className="bg-green-700 text-white px-6 py-4 shadow">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
+
         <div className="flex items-center gap-2">
           <Leaf />
           <span className="font-bold text-xl">
@@ -12,9 +14,23 @@ function Navbar() {
         </div>
 
         <div className="flex gap-6">
-          <a href="/">Home</a>
-          <a href="/dashboard">Dashboard</a>
-          <a href="/calculator">Calculator</a>
+
+          <Link to="/">
+            Home
+          </Link>
+
+          <Link to="/dashboard">
+            Dashboard
+          </Link>
+
+          <Link to="/calculator">
+            Calculator
+          </Link>
+
+          <Link to="/login">
+            Login
+          </Link>
+
         </div>
       </div>
     </nav>

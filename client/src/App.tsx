2ced0a83ac41c92ee9770700e9f3e15e363411bf
priya <priya1722006@gmail.com>
2026-06-09 19:26:20 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,7 @@
-import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="min-h-screen">
-        <h1 className="text-4xl font-bold text-center mt-20">
-          CarbonMirror AI
-        </h1>
-
-        <p className="text-center mt-4">
-          Predict • Reduce • Sustain
-        </p>
-      </div>
-    </BrowserRouter>
-  );
+  return <AppRoutes />;
 }
 
 export default App;

f5502ecb3306b929a46f57c48015c5289bfe7920
priya <priya1722006@gmail.com>
2026-06-09 19:00:13 +0530

    Update CarbonRecord.ts

--- server/src/models/CarbonRecord.ts
+++ server/src/models/CarbonRecord.ts
@@ -1,1 +1,36 @@
+import mongoose, { Schema, Document } from "mongoose";
 
+export interface ICarbonRecord extends Document {
+  userId: string;
+  transportKm: number;
+  electricityUnits: number;
+  waterUsage: number;
+  wasteKg: number;
+  foodType: string;
+  totalEmission: number;
+  sustainabilityScore: number;
+}
+
+const CarbonRecordSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true
+    },
+    transportKm: Number,
+    electricityUnits: Number,
+    waterUsage: Number,
+    wasteKg: Number,
+    foodType: String,
+    totalEmission: Number,
+    sustainabilityScore: Number
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default mongoose.model<ICarbonRecord>(
+  "CarbonRecord",
+  CarbonRecordSchema
+);

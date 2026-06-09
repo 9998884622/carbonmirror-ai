839110d51ea75ca40da5608fea3109fb25784e9b
priya <priya1722006@gmail.com>
2026-06-09 19:02:47 +0530

    Update predictionService.ts

--- server/src/services/predictionService.ts
+++ server/src/services/predictionService.ts
@@ -1,1 +1,17 @@
+export const predictFutureEmission = (
+  currentEmission: number
+) => {
+  return {
+    oneMonth:
+      currentEmission * 30,
 
+    sixMonths:
+      currentEmission * 180,
+
+    oneYear:
+      currentEmission * 365,
+
+    fiveYears:
+      currentEmission * 1825
+  };
+};

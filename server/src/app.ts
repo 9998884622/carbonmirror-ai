6b1c2f3f03646ec4333be0e523eb86f269d82270
priya <priya1722006@gmail.com>
2026-06-09 18:59:21 +0530

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,11 +4,17 @@ import helmet from "helmet";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
 
+import authRoutes from "./routes/authRoutes";
+import userRoutes from "./routes/userRoutes";
+
 const app = express();
 
 app.use(express.json());
+
 app.use(cors());
+
 app.use(helmet());
+
 app.use(morgan("dev"));
 
 app.use(
@@ -24,5 +30,9 @@ app.get("/", (_, res) => {
     message: "CarbonMirror AI API Running"
   });
 });
+
+app.use("/api/auth", authRoutes);
+
+app.use("/api/users", userRoutes);
 
 export default app;

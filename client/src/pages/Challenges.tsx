a48c3edad6b313a325e45717fa5ced1eaafd9f34
priya <priya1722006@gmail.com>
2026-06-09 20:08:38 +0530

    Create Challenges.tsx

---
+++ client/src/pages/Challenges.tsx
@@ -0,0 +1,15 @@
+function Challenges() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">
+        Eco Challenges
+      </h1>
+
+      <p className="mt-4">
+        Coming Soon...
+      </p>
+    </div>
+  );
+}
+
+export default Challenges;
